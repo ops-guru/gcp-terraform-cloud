3c878b53e24183e41294440c657637376524b97d
Marko Vlahovic <vlahovic74@gmail.com>
2019-09-19 13:46:07 -0700

    fixing a typo

--- infra-service-project/remote.tf
+++ infra-service-project/remote.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "infra-host-project" {
   backend = "remote"
   config = {
     organization = var.terraform_organization
-    workspcases = {
+    workspaces = {
       name = "infra-host-project-${var.environment}"
     }
   }
